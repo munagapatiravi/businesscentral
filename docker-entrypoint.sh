82c4bbddbdc026c000db9b53025d8af162d017a4
pavanpa <pavanpatel84@gmail.com>
2020-10-19 23:46:00 +0530

    update propeties file name

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -e
-$JBOSS_HOME/bin/standalone.sh -c standalone-full.xml -Djboss.socket.binding.port-offset=101 -P $JBOSS_HOME/bin/system.propertie
+$JBOSS_HOME/bin/standalone.sh -c standalone-full.xml -Djboss.socket.binding.port-offset=101 -P $JBOSS_HOME/bin/system.properties
